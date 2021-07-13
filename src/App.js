320d03c85d75e0beef6ce63f1c5e2a69cb628cb5
Deadshot <shujaakba2020@gmail.com>
2021-07-13 18:44:20 +0500

    Flask React Application

---
+++ src/App.js
@@ -0,0 +1,157 @@
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+  Link
+} from "react-router-dom";
+
+import {login, authFetch, useAuth, logout} from "./auth"
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/secret">Secret</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute path="/secret" component={Secret} />
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function SignUp() {
+
+}
+
+function Home() {
+  useEffect(() => {
+    fetch("/api").then(resp => resp.json()).then(resp => console.log(resp))
+  }, [])
+  return <h2>Home</h2>;
+}
+
+function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmitClick = (e)=>{
+    e.preventDefault()
+    console.log("You pressed login")
+    let opts = {
+      'username': username,
+      'password': password
+    }
+    console.log(opts)
+    fetch('/api/login', {
+      method: 'post',
+      body: JSON.stringify(opts)
+    }).then(r => r.json())
+      .then(token => {
+        if (token.access_token){
+          login(token)
+          console.log(token)          
+        }
+        else {
+          console.log("Please type in correct username/password")
+        }
+      })
+  }
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const [logged] = useAuth();
+
+  return (
+    <div>
+    <h2>Login</h2>
+    {!logged? <form action="#">
+      <div>
+        <input type="text" 
+          placeholder="Username" 
+          onChange={handleUsernameChange}
+          value={username} 
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+      </div>
+      <button onClick={onSubmitClick} type="submit">
+        Login Now
+      </button>
+    </form>
+    : <button onClick={() => logout()}>Logout</button>}
+  </div>
+  )
+}
+
+function Secret() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    authFetch("/api/protected").then(response => {
+      if (response.status === 401){
+        setMessage("Sorry you aren't authorized!")
+        return null
+      }
+      return response.json()
+    }).then(response => {
+      if (response && response.message){
+        setMessage(response.message)
+      }
+    })
+  }, [])
+  return (
+    <h2>Secret: {message}</h2>
+  )
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const [logged] = useAuth();
+
+  return <Route {...rest} render={(props) => (
+    logged
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+}
